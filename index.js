d6a8448f83fe629cb57e393cdfeab2323533e0e2
J1741 <84889065+J1741@users.noreply.github.com>
2021-08-18 18:58:05 -0400

    adding methods to classes

---
+++ index.js
@@ -0,0 +1,9 @@
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const employee = new Employee;
+const manager = new Manager;
+const engineer = new Engineer;
+const intern = new Intern;
